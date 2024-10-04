7c28f3a702c7b2368b6af55182abe0a5fb62f31d
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-10-03 21:37:46 -0400

    renderProductDetails

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,5 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import productData from "./productData.mjs";
-import productDetails from './productDetails.mjs';
-import {getParams} from './utils.mjs'
+ import productDetails from './productDetails.mjs';
+import {getParam} from './utils.mjs'
 
 const productId = getParam('prodcut');
 productDetails(productId);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -10,9 +10,21 @@ export default async function productDetails(productId) {
 }
 
 function addProductToCart(product) {
-    const cart = getLocalStorage("so-cart") || [];
-    cart.push(product);
+   // const cart = getLocalStorage("so-cart") || [];
+    //cart.push(product);
     setLocalStorage("so-cart", cart);
   }
   
-
+function renderProductDetails(){
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
+}
