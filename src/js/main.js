1d4cb58922ccba7ca8feefb30575cd93e7c2033e
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-10-11 17:22:37 -0400

    cart count to show while there is currently an item

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import productList from "./productList.mjs";
 import { renderAlerts } from "./alert.mjs";
+import { renderCartCount } from "./utils.mjs";
 
 productList(".product-list", "tents");
 renderAlerts();
+renderCartCount();
--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
 import productDetails from "./productDetails.mjs";
-import { getParam } from "./utils.mjs";
+import { getParam, renderCartCount } from "./utils.mjs";
 
 const productId = getParam("product");
 productDetails(productId);
+renderCartCount();
