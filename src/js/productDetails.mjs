32120d1f0d7b40f94c00d303a96d8c20e1348d2b
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-10-12 16:37:18 -0400

    discount on listing done

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -19,12 +19,15 @@ function addProductToCart() {
   }
   
 function renderProductDetails(){
+  
+
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
   document.querySelector("#productImage").src = product.Image;
   document.querySelector("#productImage").alt = product.Name;
   document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productDiscountPrice").innerText = product.SuggestedRetailPrice+ ' SAVE '+ (product.SuggestedRetailPrice- product.FinalPrice);
   document.querySelector("#productColorName").innerText =
     product.Colors[0].ColorName;
   document.querySelector("#productDescriptionHtmlSimple").innerHTML =
