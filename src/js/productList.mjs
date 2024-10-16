edec52c26bb5ecbc5cd01cecc0c4a4fec3ea0930
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-10-16 18:56:44 -0400

    discounts stylized

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -19,7 +19,7 @@ function productCardTemplate(product) {
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
       <p class="product-card__price">$${product.FinalPrice}</p>
-       <p class="product-card__discount" style="display: ${discounted}; color: red;">Save $${discount}</p>
+       <p class="product-card__discount product_discount" style="display: ${discounted};">Save $${discount}</p>
     </li>`;
   }
 
