1e94d2f28332a970917cbaa72b9c4f593227bfc6
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-10-16 19:49:21 -0600

    finishing team4

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,8 @@
 import productDetails from "./productDetails.mjs";
 import { getParam, renderCartCount } from "./utils.mjs";
+import{loadHeaderFooter} from "./utils.mjs"
 
+loadHeaderFooter();
 const productId = getParam("product");
 productDetails(productId);
 renderCartCount();
