43f3046b49bad5d2b9b1a1cb865faddc5ca0ece4
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-10-23 19:59:46 -0600

    changed title

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -26,8 +26,7 @@ function productCardTemplate(product) {
 export default async function productList(selector, category){
     const element= document.querySelector(selector);
     const products= await getData(category);
-    
-    console.log(products);
-    
+    const categoryTitle = document.getElementById("category-title");
+    categoryTitle.innerText = category.charAt(0).toUpperCase() + category.slice(1);
     renderListWithTemplate(productCardTemplate, element, products);
 }
