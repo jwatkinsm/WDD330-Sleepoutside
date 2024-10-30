6874233e3ec3e1034d8a962085f8b181781ba219
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-10-30 16:47:18 -0400

    searchbar html added

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,19 @@ import { renderAlerts } from "./alert.mjs";
 import { renderCartCount } from "./utils.mjs";
 import{loadHeaderFooter} from "./utils.mjs"
  
+function handleSearch(event) {
+    event.preventDefault(); 
+    const searchInput = document.querySelector('#search').value.trim();
+    
+    if (searchInput) {
+      window.location.href = `product-listing/index.html?search=${encodeURIComponent(searchInput)}`;
+    }
+  }
+  const searchForm = document.querySelector('#search');
+  if (searchForm) {
+    searchForm.addEventListener('submit', handleSearch);
+  }
+
 loadHeaderFooter();
 renderAlerts();
 renderCartCount();
