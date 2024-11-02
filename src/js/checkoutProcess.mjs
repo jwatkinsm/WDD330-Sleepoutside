fdb3ba5c91e00a9a4b5b07b49e5b765fad7abdcf
Emilio Ordonez Guerrero <emilioordgue23@gmail.com>
2024-11-01 19:31:44 -0600

    packageItems, calcItemSummary, displayOrderTotals

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -1,5 +1,17 @@
 import { getLocalStorage } from "./utils.mjs";
 
+function packageItems(items) {
+    const simpleItems = items.map((item) => {
+        return {
+            id: item.id,
+            price: item.FinalPrice,
+            name: item.Name, 
+            quantity: 1,
+        };
+    });
+    return simpleItems;
+}
+
 const checkoutProcess = {
     key:"",
     outputSelector: "",
@@ -14,15 +26,26 @@ const checkoutProcess = {
         this.list= getLocalStorage(key);
         this.calculateItemSummary();
     },
-    calculateIemSummary: function(){
+    calculateItemSummary: function(){
+        const summaryEl = document.querySelector(this.outputSelector + " #cartTotal");
+        const itemsNumberEl = document.querySelector(this.outputSelector + " #num-items");
+        itemsNumberEl.innerText = this.list.length;
 
+        const amounts = this.list.map((item) => item.FinalPrice);
+        this.itemTotal = amounts.reduce((sum, item) => sum + item);
+        summaryEl.innerText = "$" + this.itemTotal;
     },
 
     calculateOrdertotal: function(){
 
     },
     displayOrderTotals: function(){
-
+        const orderTotal = document.querySelector(this.outputSelector + " #orderTotal");
+        const shipping = document.querySelector(this.outputSelector + " #shipping");
+        const tax = document.querySelector(this.outputSelector + " #tax");
+        orderTotal.innerText = "$" + this.orderTotal;
+        shipping.innerText = "$" + this.shipping;
+        tax.innerText = "$" + this.tax;
     }
 }
 export default checkoutProcess;
