34a4e91801dd107cf1fa5c830a435234ea8cb8a9
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-11-09 19:02:15 -0500

    search solution?

--- src/js/search-result.js
+++ src/js/search-result.js
@@ -1,12 +1,12 @@
 import { loadHeaderFooter, renderListWithTemplate, getParam } from "./utils.mjs";
-import ExternalServices from "./ExternalServices.mjs";
+import getProductsByCategory from "./externalServices.mjs";
 
 
 
 loadHeaderFooter();
 
 const box = document.querySelector(".product-list");
-const services = new ExternalServices();
+const services = new getProductsByCategory;
 
 async function getSearchResults() {
   try {
@@ -54,4 +54,18 @@ async function getSearchResults() {
     box.innerHTML = "<p>There was an error. Please try again later.</p>";
   }
 }
+
+function productCardTemplate(product) {
+  // populates products to the product page
+  return `
+    <li class="product-card">
+        <a href="/product_pages/index.html?product=${product.Id}">
+            <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}">
+            <h3 class="card__brand">${product.Brand.Name}</h3>
+            <h2 class="card__name">${product.Name}</h2>
+            <p class="product-card__price">$${product.FinalPrice}</p>
+        </a>
+    </li>`;
+}
+
 getSearchResults();
--- src/js/search.js
+++ src/js/search.js
@@ -1,8 +1,8 @@
-const search = document.querySelector("#searchinput");
+const search = document.querySelector("#searchInput");
 const searchButton = document.querySelector("#searchButton");
 const clearButton = document.querySelector("#clearButton");
 
-searchButton.addEventListener("click", async (e) => {
+searchButton.addEventListener("click", function(event){
   e.preventDefault();
 
   let searchValue = search.value;
@@ -10,13 +10,13 @@ searchButton.addEventListener("click", async (e) => {
   if (searchValue && searchValue.length > 0) {
     searchValue == searchValue.trim().toLowerCase();
     // redirect to the products page with the search query
-    window.location.href = `products/index.html?search=${searchValue}`;
+    window.location.href = `product/index.html?search=${searchValue}`;
   } else {
     // do nothing
   }
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", function(event) {
   search.value = "";
   search.focus();
 });
