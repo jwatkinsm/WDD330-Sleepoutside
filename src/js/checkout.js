c71c287179685d7fd5358c73907a8816c1ff5d80
jwatkinsm <49845925+jwatkinsm@users.noreply.github.com>
2024-11-15 23:36:58 -0500

    team 8 complete

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,4 +1,4 @@
-import { loadHeaderFooter, alertMessage} from "./utils.mjs";
+import { loadHeaderFooter} from "./utils.mjs";
 import checkoutProcess from "./checkoutProcess.mjs";
 
 loadHeaderFooter();
@@ -12,46 +12,9 @@ document
     checkoutProcess.calculateOrdertotal.bind(checkoutProcess)
   );
 
-  document.querySelector('#checkout')
-  .addEventListener('click', (e) => {
+  document.forms["checkout"].addEventListener("submit", (e) => {
     e.preventDefault();
-    var myForm = document.forms[0];
-    var chk_status = myForm.checkValidity();
-    myForm.reportValidity();
-    const form = document.getElementById("checkout-form");
-    const zip = myform.zip.value;
-    const cardNumber = form.cardNumber.value;
-    const expiration = form.expiration.value;
-    const code = form.code.value;
+    // e.target would contain our form in this case
+    checkoutProcess.checkout(e.target);
 
-    const zipPattern = /^\d{5}$/;
-    const cardPattern = /^\d{16}$/;
-    const expirationPattern = /^(0[1-9]|1[0-2])\/\d{2}$/;
-    const codePattern = /^\d{3}$/;
-
-    let alertList = [];
-
-    if (!zipPattern(zip)) {
-      alertList.push("Invalid zip code");
-    }
-    if (!cardPattern.test(cardNumber)) {
-      alertList.push("Invalid card number");
-    }
-    if (!expirationPattern.test(expiration)) {
-      alertList.push("Invalid expiration date");
-    }
-    if (!codePattern.test(code)) {
-      alertList.push("Invalid security code");
-    }
-
-    if (alertList.length > 0) {
-      alertList.forEach((message) => {
-        alertMessage(message);
-      });
-      return;
-    }
-
-    if(chk_status) 
-      checkoutProcess.checkout();
-    return;
   });
